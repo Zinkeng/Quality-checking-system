daf99ca71aedccbed0bfa714eb62450418e44fa7
Zinkeng <ajongafaczinkeng@gmail.com>
2023-05-11 10:27:20 +0100

    First commit

---
+++ index.py
@@ -0,0 +1,22 @@
+
+price = 1000000
+good_credit = False
+
+if good_credit:
+    print("Bring down price by 10%")
+    down_payment = 0.1 * price
+    
+else:
+    print("Bring down price by 20%")
+    down_payment = 0.2 * price
+    
+    
+print(f"down_payment is: {down_payment} ")
+
+
+
+
+    
+
+
+
---
+++ learn.py
@@ -0,0 +1,15 @@
+
+Weight = float(input("Enter weight "))
+print(Weight)
+unit = input("(L)bs or Kg? ")
+print(unit)
+
+if unit.upper() == 'L':
+    weight_kg = 0.45 * Weight
+    print(f"your weight in kilograms is {weight_kg}: " )
+
+
+elif unit.upper() == 'KG':
+    weight_lbs = float(Weight)/0.45
+    print(f"your weight in pounds is {weight_lbs}: " )
+ 
